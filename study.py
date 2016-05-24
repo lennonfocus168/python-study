96bc5a3be31f2ce0995f2aa08555406c171cdd17
zhenghuan <zeghaun@163.com>
2016-05-24 10:20:26 +0800

    hah

--- study.py
+++ study.py
@@ -1,1 +1,14 @@
-print("hellowoer")
+import urllib
+
+import urllib.request
+
+data = {}
+data['word'] = '1'
+
+url_values = urllib.parse.urlencode(data)
+url = "http://www.baidu.com/s?"
+full_url = url + url_values
+
+data = urllib.request.urlopen(full_url).read()
+data = data.decode('UTF-8')
+
