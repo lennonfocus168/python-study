6123f64672a0e7770dc4499c8ebd8856c48b1e86
zhenghuan <zeghaun@163.com>
2016-05-27 15:05:58 +0800

    Merge remote-tracking branch 'origin/dev' into dev
    # Conflicts:
    #	test/__init__.py

--- test/main.py
+++ test/main.py
@@ -1,10 +1,10 @@
+
 headers = [('Content-Type', 'application/x-www-form-urlencoded'), ('Connection', 'keep-alive'), ('DNT', '1'),
            ('Cache-Control', 'no-cache'), ('User-Agent',
                                            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36'),
            ('Accept-Language', 'zh-CN,zh;q=0.8,en;q=0.6'),
            ('Referer', 'http//acm.fzu.edu.cn/login.php?dir=L2luZGV4LnBocA=='),
            ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'),
+           ('Accept-Encoding', 'gzip, deflate'),
            ('Host', 'acm.fzu.edu.cn'), ('Origin', 'http//acm.fzu.edu.cn'), ('Upgrade-Insecure-Requests', ' 1'),
-           ('Pragma', 'no-cache')]
-
-
+           ('Cookie', 'FASAST=7vbtpgjviv872vsg60me6n8086')]
