9ac9476a7719eb65f5646252097996043e220c27
zhenghuan <zeghaun@163.com>
2016-07-15 10:53:48 +0800

    1.知乎爬虫 增加集合爬虫和问题爬虫

--- test/study.py
+++ test/study.py
@@ -1,9 +1,15 @@
 import re
 
-content = '<div class="zm-item" data-za-module="AnswerItem"><h2 class="zm-item-title"><a target="_blank" href="/question/20399991">女生的美腿是怎样炼成的？成长期要注意什么？</a></h2>'
+content = ''
 
-re_str = '<a target="_blank" href="(.*?)">(.*?)</a>'
-searchObj = re.findall(re_str, content, re.M | re.I)
-print(searchObj)
 
-print('cdscsdcc'.find('Q'))
+def scopetest():
+    global var
+    var = 6
+    print('test', var)  #
+
+
+var = 5
+print(var)
+scopetest()
+print(var)
