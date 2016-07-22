16ec48cc02c4bc5ae2f5dd9efe591a4435a6e3c5
zhenghuan <zeghaun@163.com>
2016-07-22 14:44:30 +0800

    1.post 提交可以

--- test/hello.py
+++ test/hello.py
@@ -13,52 +13,20 @@ from urllib.parse import urlsplit
 
 from setuptools.compat import BytesIO
 
-spider_url = r'https://www.zhihu.com/question/32674678'
+spider_url = r'https://www.btctrade.com/'
 # spider_url = r'http://bbs.fengniao.com/forum/8982080.html'
-file_path = r"E:\image\\"
 
-headers = [('User-Agent',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36'),
-           ('Accept-Language', 'zh-CN,zh;q=0.8,en;q=0.6'),
-           ('Referer', spider_url),
-           ('Host', 'www.zhihu.com')]
+url = r'http://localhost:8080/test/3223/323'
+data = {"name": "zeg104935408632"}
+post = urllib.parse.urlencode(data)
+post = post.encode('utf-8')
 
+request = urllib.request.Request(url, post)
+print(str(request.data))
+response = urllib.request.urlopen(request)
+content = response.read().decode('utf-8')
 
-def get_html(pageurl, encoding='utf-8'):
-    # 获取目标网站任意一个页面的html代码
-    request = urllib.request.Request(pageurl)
-    response = urllib.request.urlopen(request)
-    if response.info().get('Content-Encoding') == 'gzip':
-        gzip_f = gzip.GzipFile(fileobj=BytesIO(response.read()))
-        result = gzip_f.read()
-    else:
-        result = response.read()
-    return result.decode(encoding)
+print(content)
 
-
-content = get_html(spider_url)
-with open(r"E:\1.html", "r", encoding='utf-8') as fw:
-    try:
-        fw.write(content)
-    except Exception as e:
-        print(e)
-    finally:
-        fw.close()
-
-
-re_str = 'img .*?src="(http://.*?.jpg)"'
-searchObj = re.findall(re_str, content, re.M | re.I)
-
-if not os.path.exists(file_path):
-    os.makedirs(file_path)
-
-for img_url in searchObj:
-    try:
-        img_data = urllib.request.urlopen(img_url).read()
-        file_name = basename(urlsplit(img_url)[2])
-        output = open(file_path + file_name, 'wb')
-        print(file_path + file_name)
-        output.write(img_data)
-        output.close()
-    except Exception as e:
-        print(e)
+with open(r"E:\1.html", "w", encoding='utF-8') as output:
+    output.write(content)
--- test/main.py
+++ test/main.py
@@ -2,6 +2,7 @@
 import re
 
 # spider_url = r'https://www.zhihu.com/question/37006507'
+
 spider_url = r'http://bbs.fengniao.com/forum/8982080.html'
 collection_url = r'https://www.zhihu.com/collection/62864589'
 # collection_url = r'https://www.zhihu.com/collection/101134785'  # 自己
@@ -13,7 +14,7 @@ col_re_str = '<a target="_blank" href="(.*?)">(.*?)</a>'
 col_list = []
 page = 1
 read = open("E:\\1.html", "r", encoding='utf-8')
-content = read.read();
+content = read.read()
 url_list = set(re.findall(re_str, content, re.M | re.I))
 
 print(url_list)
@@ -21,6 +22,6 @@ print(len(url_list))
 
 end_set = {}
 
-for url in url_list:
-    if str(url).endswith(".png"):
-        print(url)
+for i in range(10):
+    print(i)
+
