de0bc9f3f37bff5d49df38b7ee94e1df8827d101
zhenghuan <zeghaun@163.com>
2016-07-22 17:37:38 +0800

    1.知乎 是https的不好抓包。。一直提示参数错误，根据spring那边经验，是entity没弄对。。所以不想研究了

--- test/hello.py
+++ test/hello.py
@@ -6,20 +6,36 @@ import json
 
 from urllib.parse import urlsplit
 
+import time
+
 spider_url = r'https://www.btctrade.com/'
 # spider_url = r'http://bbs.fengniao.com/forum/8982080.html'
-headers = {'Content-Type': 'application/json', 'Accept-Encoding': 'gzip, deflate',
-           'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive',
-           'Upgrade-Insecure-Requests': '1', 'Host': 'zhihu.com',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36',
-           'DNT': '1'}
+str = "%a, %d %b %Y %H:%M:%S GMT"
+Date = time.strftime(str, time.gmtime(time.time()))
+expires = time.strftime(str, time.gmtime(time.time() - 365 * 24 * 60 * 60))
 
-url = r'http://localhost:8080/v0.1/demo'
-data = {"name": "zeg104935408632"}
-print(str(data))
-print(json.dumps(data))
-request = urllib.request.Request(url, json.dumps(data).encode(encoding='utf-8'), headers=headers)
+headers = {"Cache-Control": "no-store",
+           "Connection": "keep-alive",
+           "Content-Encoding": "gzip",
+           "Content-Length": "12667",
+           "Content-Security-Policy": "default-src *; img-src * data:; frame-src 'self' *.zhihu.com getpocket.com note.youdao.com; script-src 'self' *.zhihu.com *.google-analytics.com zhstatic.zhihu.com res.wx.qq.com 'unsafe-eval'; style-src 'self' *.zhihu.com 'unsafe-inline'",
+           "Content-Type": "application/json",
+           "Date": Date,
+           "Pragma": "no-cache",
+           "Server": "Qnginx/1.1.1",
+           "Set-Cookie": "_xsrf=; Domain=zhihu.com; expires=" + expires + "; Path=/",
+           "Vary": "Accept-Encoding",
+           "X-Frame-Options": "DENY",
+           "X-NWS-LOG-UUID": "7a8162f6-e654-4b3d-affc-1bf31c6172d7",
+           "X-Req-ID": "2C4353585791D89E",
+           "X-Za-Response-Id": "0d052e41d5734e6fb712286b6412613a"}
+
+print(json.dumps(headers))
+
+url = r'https://www.zhihu.com/node/QuestionAnswerListV2'
+data = {"method": "next", "param": {"url_token": 20702054, "pagesize": 10, "offset": 20}}
+
+request = urllib.request.Request(url, json.dumps(data).encode(encoding='utf-8'), method='POST')
 response = urllib.request.urlopen(request)
 content = response.read().decode('utf-8')
 
--- test/study.py
+++ test/study.py
@@ -1,15 +1,12 @@
-headers = {('Content-Type', 'application/json'), ('Connection', 'keep-alive'), ('DNT', '1'),
-           ('Cache-Control', 'no-cache'), ('User-Agent',
-                                           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36'),
-           ('Accept-Language', 'zh-CN,zh;q=0.8,en;q=0.6'),
-           ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'),
-           ('Accept-Encoding', 'gzip, deflate'),
-           ('Host', 'zhihu.com'), ('Upgrade-Insecure-Requests', '1')}
+import json
 
-head = {}
-print(type(head))
-for key, value in headers:
-    head[key] = value
-    pass
+import time
 
-print(head)
+data = {"name": "zeg104935408632"}
+
+str = "%a, %d %b %Y %H:%M:%S GMT"
+print(time.strftime(str, time.localtime(time.time())))
+print(time.strftime(str, time.localtime(time.time() - 365 * 24 * 60 * 60)))
+print(time.strftime(str, time.gmtime(time.time())))
+
+'Fri, 22 Jul 2016 08:31:29 GMT'
