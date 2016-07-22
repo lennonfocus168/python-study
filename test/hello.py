232e7ffcfec5fb223c9f7e1fc6c37935273efc0c
zhenghuan <zeghaun@163.com>
2016-07-22 15:59:59 +0800

    1.post 提交必须有对json的处理

--- test/hello.py
+++ test/hello.py
@@ -1,28 +1,25 @@
 # coding=utf-8
-import gzip
-import http.cookiejar
-import os
 import urllib
 import urllib.request
 import urllib.request
+import json
 
-import re
-
-from os.path import basename
 from urllib.parse import urlsplit
-
-from setuptools.compat import BytesIO
 
 spider_url = r'https://www.btctrade.com/'
 # spider_url = r'http://bbs.fengniao.com/forum/8982080.html'
+headers = {'Content-Type': 'application/json', 'Accept-Encoding': 'gzip, deflate',
+           'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive',
+           'Upgrade-Insecure-Requests': '1', 'Host': 'zhihu.com',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36',
+           'DNT': '1'}
 
-url = r'http://localhost:8080/test/3223/323'
+url = r'http://localhost:8080/v0.1/demo'
 data = {"name": "zeg104935408632"}
-post = urllib.parse.urlencode(data)
-post = post.encode('utf-8')
-
-request = urllib.request.Request(url, post)
-print(str(request.data))
+print(str(data))
+print(json.dumps(data))
+request = urllib.request.Request(url, json.dumps(data).encode(encoding='utf-8'), headers=headers)
 response = urllib.request.urlopen(request)
 content = response.read().decode('utf-8')
 
--- test/study.py
+++ test/study.py
@@ -1,32 +1,15 @@
-import re
+headers = {('Content-Type', 'application/json'), ('Connection', 'keep-alive'), ('DNT', '1'),
+           ('Cache-Control', 'no-cache'), ('User-Agent',
+                                           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36'),
+           ('Accept-Language', 'zh-CN,zh;q=0.8,en;q=0.6'),
+           ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'),
+           ('Accept-Encoding', 'gzip, deflate'),
+           ('Host', 'zhihu.com'), ('Upgrade-Insecure-Requests', '1')}
 
-content = ''
+head = {}
+print(type(head))
+for key, value in headers:
+    head[key] = value
+    pass
 
-
-class MyWith(object):
-    def __enter__(self):
-        print("Enter with")
-        return self  # 返回对象给as后的变量
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        # 关闭资源等
-        if exc_traceback is None:
-            print("Exited without Exception")
-            return True
-        else:
-            print("Exited with Exception")
-            return False
-
-
-def test_with():
-    with MyWith() as my_with:
-        print("running my_with")
-    print("------分割线-----")
-    with MyWith() as my_with:
-        print("running before Exception")
-        raise Exception
-        print("running after Exception")
-
-
-if __name__ == '__main__':
-    test_with()
+print(head)
