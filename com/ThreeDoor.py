3c23bf51b0cf837c14c886929fa3bdaeab194649
zhenghuan <zeghaun@163.com>
2016-07-27 11:20:18 +0800

    1.三门问题

--- com/ThreeDoor.py
+++ com/ThreeDoor.py
@@ -1,6 +1,7 @@
+# 三门问题（Monty Hall problem）
 import random
 
-temp = [0, 0, 1]
+data = [0, 0, 1]
 
 n = 100000  # 实验进行次数
 size = 3  # 步长
@@ -8,7 +9,6 @@ result = 0  # 原来结果
 rev = 0  # 换后结果
 
 for i in range(n):
-    data = temp
     random.shuffle(data)
     k = random.randint(0, size - 1)
     # 寻找data[j]==0 且j!=k 找到那只羊
